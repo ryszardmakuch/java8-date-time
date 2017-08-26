ec98c1d107badb817a580db40e15ac271515b34b
Ryszard Makuch <ryszard.makuch@outlook.com>
2017-08-26 23:46:26 +0200

    [Master] Solved Years.

--- src/main/java/ski/java8/time/Years.java
+++ src/main/java/ski/java8/time/Years.java
@@ -1,15 +1,23 @@
 package ski.java8.time;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
 
 public class Years {
 
     public static boolean isLeap(Date date) {
-        return false;
+        return LocalDateTime
+                .ofInstant(date.toInstant(), ZoneId.systemDefault())
+                .toLocalDate()
+                .isLeapYear();
     }
 
     public static LocalDate lastYearSameDay(LocalDate thisYear) {
-        return null;
+        return thisYear
+                .minusYears(1)
+                .with(TemporalAdjusters.nextOrSame(thisYear.getDayOfWeek()));
     }
 }
