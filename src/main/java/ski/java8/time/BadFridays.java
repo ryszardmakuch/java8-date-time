7320e069fc2dbf6eedc8c849c4cf9219403aedae
Ryszard Makuch <ryszard.makuch@outlook.com>
2017-08-26 23:05:52 +0200

    [Master] Solved BadFridays.

--- src/main/java/ski/java8/time/BadFridays.java
+++ src/main/java/ski/java8/time/BadFridays.java
@@ -1,10 +1,24 @@
 package ski.java8.time;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class BadFridays {
-
     public static LocalDate next(LocalDate from) {
-        return null;
+        LocalDate thirteenthDayOfMonth = findNext13thOfMonth(from);
+        while (thirteenthDayOfMonth.getDayOfWeek() != DayOfWeek.FRIDAY) {
+            thirteenthDayOfMonth = thirteenthDayOfMonth.plusMonths(1);
+        }
+        return thirteenthDayOfMonth;
+    }
+
+    private static LocalDate findNext13thOfMonth(LocalDate from) {
+        LocalDate next13thOfMonth;
+        if (from.getDayOfMonth() >= 13) {
+            next13thOfMonth = from.plusMonths(1).withDayOfMonth(13);
+        } else {
+            next13thOfMonth = from.withDayOfMonth(13);
+        }
+        return next13thOfMonth;
     }
 }
